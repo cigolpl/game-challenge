445825de9ae53b80a4870e8d8f4373ed23a1d368
Mateusz Rzepa <mrzepa89@gmail.com>
2018-07-23 09:45:20 +0200

    remove bash file

--- bash.sh
+++
@@ -1,4 +0,0 @@
-curl -XPOST -H "Content-Type: application/json" http://localhost:3000/new_game
-curl -XPOST -H "Content-Type: application/json" -d '{"number":60}' http://localhost:3000/move
-curl -XPOST -H "Content-Type: application/json" -d '{"number":0}' http://localhost:3000/move
-curl -XPOST -H "Content-Type: application/json" -d '{"number":0}' http://localhost:3000/move
